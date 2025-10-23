741ae5e30cba34d9803939af7210355290b65ada
L1verly <artemartem7975@gmail.com>
2025-10-23 13:11:39 +0000

    campground model init

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+//DB connection through mongoose ODM
+main()
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/YelpCampDB");
+}
+
+//Express app setup
 const app = express();
 const port = 3000;
 
@@ -11,5 +24,16 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.render("home");
+});
+
+app.get("/newcampground", async (req, res) => {
+  const campground = new Campground({
+    title: "My Backyard",
+    price: 0,
+    description: "Cheap camping!",
+    location: "NY",
+  });
+  await campground.save();
+  res.send(campground);
 });
