ab8c68f625ceaf672329f9699f7355e7915694a5
L1verly <artemartem7975@gmail.com>
2025-10-24 11:48:34 +0000

    Added index, new, editm show views

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 //DB connection through mongoose ODM
@@ -18,6 +19,8 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
@@ -27,13 +30,39 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/newcampground", async (req, res) => {
-  const campground = new Campground({
-    title: "My Backyard",
-    price: 0,
-    description: "Cheap camping!",
-    location: "NY",
-  });
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", async (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
   await campground.save();
-  res.send(campground);
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    req.body.campground,
+    { runValidators: true, new: true }
+  );
+  res.redirect(`/campgrounds/${campground._id}`);
 });
