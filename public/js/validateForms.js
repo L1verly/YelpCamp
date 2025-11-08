b59485b1d7948349389a9b807d42a9c240d2b606
L1verly <artemartem7975@gmail.com>
2025-11-08 14:30:29 +0100

    ValidateForms fix

--- public/js/validateForms.js
+++ public/js/validateForms.js
@@ -1,15 +1,14 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
+(function () {
   "use strict";
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll(".needs-validation");
-
+  console.log(forms);
   // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
+  Array.from(forms).forEach(function (form) {
     form.addEventListener(
       "submit",
-      (event) => {
+      function (event) {
         if (!form.checkValidity()) {
           event.preventDefault();
           event.stopPropagation();
