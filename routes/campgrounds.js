563c3cdbf2706ddff457e30ad5b460bf62699052
L1verly <artemartem7975@gmail.com>
2025-11-11 16:57:40 +0100

    Adding images upload feature for camps

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const upload = require("../cloudinary");
 
 router
   .route("/")
   .get(campgrounds.index)
-  .post(isLoggedIn, validateCampground, campgrounds.createOne);
+  .post(
+    isLoggedIn,
+    validateCampground,
+    upload.array("campground[images]"),
+    campgrounds.createOne
+  );
 
 router.get("/new", isLoggedIn, campgrounds.newForm);
 
--- schemas.js
+++ schemas.js
@@ -3,7 +3,12 @@ const campgroundSchema = joi.object({
   campground: joi
     .object({
       title: joi.string().required(),
-      image: joi.string().required(),
+      images: joi
+        .object({
+          url: joi.string(),
+          filename: joi.string(),
+        })
+        .required(),
       price: joi.number().required().min(0),
       description: joi.string().required(),
       location: joi.string().required(),
